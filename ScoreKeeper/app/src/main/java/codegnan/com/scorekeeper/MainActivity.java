c7c06d85e083839447ec9e39d373a9afd346b89d
PAVAN KUMAR REDDY TADI <pavankumart46@gmail.com>
2020-07-29 19:32:07 +0530

    App

--- ScoreKeeper/app/src/main/java/codegnan/com/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/codegnan/com/scorekeeper/MainActivity.java
@@ -3,18 +3,34 @@ package codegnan.com.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Connecting the textview on activity_main.xml to the tv object (TextView) on
+        // the MainActivity.java
+        tv = findViewById(R.id.result);
     }
 
     public void decrementScore(View view)
     {
+        count--;
+        tv.setText(String.valueOf(count));
+    }
 
+    public void incrementScore(View view)
+    {
+        count++;
+        tv.setText(""+count);
     }
 }
